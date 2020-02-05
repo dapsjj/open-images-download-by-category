7309bdf3ea035e45a0304e0aa99482f17ed2cdc2
Rhaegal <dapsjj@qq.com>
2020-02-05 15:16:45 +0800

    Create put_the_specified_category_image_into_CSV.py

---
+++ put_the_specified_category_image_into_CSV.py
@@ -0,0 +1,19 @@
+
+import csv
+
+with open(r'./OID/csv_folder_nl/train-annotations-machine-imagelabels_bk.csv','r', encoding='utf-8') as f,open(r'./OID/csv_folder_nl/Wheel.csv','w',newline='',encoding='utf-8') as f_out:
+    writer = csv.writer(f_out)
+    alllist=[]
+    i=1
+    for row in f:
+        row = row.replace('\n', '')
+        line = row.split(',')
+        '''
+        /m/04_bsf Wheelie
+        /m/083wq Wheel
+        '''
+        if line[2] =='/m/083wq':
+            print(i)
+            alllist.append(line)
+            i += 1
+    writer.writerows(alllist)
